c924dfca8f533569b5d5006dc8ff777640e058ff
ryan <ryankurte@users.noreply.github.com>
2023-08-25 07:38:38 +1200

    ui changes per ledger functional review (#76)

--- fw/src/ui/app_info.rs
+++ fw/src/ui/app_info.rs
@@ -1,6 +1,5 @@
 // Copyright (c) 2022-2023 The MobileCoin Foundation
 
-use emstr::{helpers::Hex, EncodeStr};
 use rand_core::{CryptoRng, RngCore};
 
 use nanos_sdk::buttons::ButtonEvent;
@@ -9,13 +8,10 @@ use nanos_ui::{
     screen_util,
 };
 
-use ledger_mob_core::{
-    apdu::app_info::AppFlags,
-    engine::{Driver, Engine},
-};
+use ledger_mob_core::engine::{Driver, Engine};
 
 use super::{clear_screen, UiResult};
-use crate::consts::{app_flags, APP_VERSION, BUILD_TIME};
+use crate::consts::{BUILD_TIME, GIT_VERSION};
 
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub struct AppInfo {}
@@ -34,36 +30,12 @@ impl AppInfo {
         }
     }
 
-    pub fn render<D: Driver, R: RngCore + CryptoRng>(&self, engine: &Engine<D, R>) {
-        let mut buff = [0u8; 32];
-
+    pub fn render<D: Driver, R: RngCore + CryptoRng>(&self, _engine: &Engine<D, R>) {
         // Clear screen
         clear_screen();
 
-        let state = engine.state();
-        let s = u16::from_be_bytes([state.state() as u8, state.value() as u8]);
-
-        let mut flags = app_flags();
-
-        if engine.is_unlocked() {
-            flags |= AppFlags::UNLOCKED;
-        }
-
-        let state_str = emstr::write!(
-            &mut buff[..],
-            "S: 0x",
-            Hex(&s.to_be_bytes()),
-            " F: 0x",
-            Hex(&flags.bits().to_be_bytes())
-        )
-        .map(|n| core::str::from_utf8(&buff[..n]));
-
-        let state_str = match state_str {
-            Ok(Ok(v)) => v,
-            _ => "INVALID_UTF8",
-        };
-
-        [APP_VERSION, BUILD_TIME, state_str].place(Location::Middle, Layout::Centered, false);
+        // Show git version and build time
+        [GIT_VERSION, BUILD_TIME].place(Location::Middle, Layout::Centered, false);
 
         // Update screen
         screen_util::screen_update();
