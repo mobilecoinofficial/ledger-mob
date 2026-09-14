b17aa0f3274ad6fcca244573b4c19fe3624eb210
ryan <120750323+ryan-sentz@users.noreply.github.com>
2026-09-14 12:58:54 +1200

    add sync approval page to the stax app

--- fw/src/main.rs
+++ fw/src/main.rs
@@ -86,6 +86,16 @@ extern "C" fn sample_main() {
 
     let mut redraw = true;
 
+    // Bind the comm to the nbgl
+    #[cfg(not(any(target_os = "nanosplus", target_os = "nanox")))]
+    ledger_device_sdk::nbgl::init_comm(&mut comm);
+
+    #[cfg(feature = "debug")]
+    ledger_device_sdk::log::debug!(
+        "Start app: {} v{} (git: {}, build: {})",
+        APP_NAME, APP_VERSION, GIT_VERSION, BUILD_TIME
+    );
+
     // non-nvm fog ID global must be pre-initialised
     #[cfg(not(feature = "nvm"))]
     platform::platform_set_fog_id(&FogId::MobMain);
@@ -201,7 +211,7 @@ extern "C" fn sample_main() {
 
         // Redraw UI on state change
         if redraw {
-            ui.render(&*engine);
+            ui.render(&mut *engine);
             redraw = false;
         }
     }
